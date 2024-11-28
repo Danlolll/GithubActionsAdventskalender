57dd4f1afb16852e30112aedba2263662950eb31
Danlo <daniel.jenner1@gmx.at>
2024-11-28 08:42:49 +0100

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module htl.steyr.githubactionsadventskalender {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens htl.steyr.githubactionsadventskalender to javafx.fxml;
+    exports htl.steyr.githubactionsadventskalender;
+}
